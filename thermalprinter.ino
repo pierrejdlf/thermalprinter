200d2ca112930b2eedb83eb3bef5b93750fe5e7c
Pierre JdlF <pierre.jdlf@gmail.com>
2014-10-24 23:58:24 +0200

    plugging things

--- thermalprinter.ino
+++ thermalprinter.ino
@@ -8,11 +8,15 @@
 // change this depending on the number of steps
 // per revolution of your motor
 #define motorSteps 20
+
 // stepper pins
 #define motorPin1 8
 #define motorPin2 9
 #define motorPin3 10
 #define motorPin4 11
+
+// temperature & LED pins
+#define tPin 0
 #define ledPin 13
 
 // thermal head pins
@@ -22,10 +26,11 @@
 #define LAT 4
 #define DATA 5
 #define CLK 6
+#define VOLTSWITCH 12
 
-boolean debug = true;
+boolean debug = false;
 boolean printing = false;
-boolean onetime = true;
+boolean goprint = true;
 
 int t = 1; // loop for thermal head clock/data/...
 
@@ -33,13 +38,16 @@ int t = 1; // loop for thermal head clock/data/...
 uint8_t linedata[] = {
   0x11,0x00,0x22};
 
-int TCLOCK = 10; // clock period in microseconds
+int TCLOCK = 1; // clock period in microseconds
+int MAXTEMP = 27; // max Temp°C before turning everything off
 
 // durations as clock cycles
 int NLAT = 432; // one dot data every clock period before releasing LATCH
-int NSTB = 5; // means 5*10*2 = during 100 microseconds !
-int NWAIT = 10; // lets wait 10 cycles between events 
+int NSTB = 700; // MAX is 670 microseconds
+int NWAIT = 25; // lets wait some cycles between events 
 int NTOTAL = NLAT + 3*NSTB + 5*NWAIT;
+
+long starttime = micros();
 
 // initialize of the Stepper library:
 Stepper myStepper(motorSteps,motorPin1,motorPin2,motorPin3,motorPin4); 
@@ -51,16 +59,28 @@ void setup() {
   myStepper.setSpeed(150);
   // Initialize the Serial port:
   Serial.begin(9600);
-  // set up the LED pin:
+
+  // set up pin modes:
   pinMode(ledPin, OUTPUT);
+  pinMode(STB1, OUTPUT);
+  pinMode(STB2, OUTPUT);
+  pinMode(STB3, OUTPUT);
+  pinMode(LAT, OUTPUT);
+  pinMode(DATA, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  pinMode(VOLTSWITCH, OUTPUT);
+
   // blink the LED:
   blink(3);
-  Serial.println("Setup done.");
+  Serial.println("Printer setup done. Welcome.");
+  myStepper.step(-20);
+
+  getTemperature(true);
 }
 
 ////////////////////////////////////////////////////////
 void loop() {
-  ////////////////////////////////// STEPPER
+  ////////////////////////////////// STEPPER TESTS
   //  Serial.println("Forward");
   //  myStepper.step(700); // Step forward
   //  delay(300);
@@ -71,68 +91,82 @@ void loop() {
   //  blink(7);
 
   ////////////////////////////////// THERMAL HEAD CYCLE
+  if(getTemperature(false) >= MAXTEMP) {
+    Serial.println("========= TEMPERATURE WARNING !! aborting all");
+    resetPrinter(); // will set printing=false and stop Vh current
+    blink(10);
+    delay(5000);
+  }
+
   if(printing) {
-
-    delay(900);
-
     if(t==1) {
       blink(1);
       Serial.println("Printing cycle launch in 1 seconds !");
       delay(1000);
       blink(2);
     }
-    Serial.print("Printing cycle k:");
-    Serial.println(t);
+    if(t%1000000==0) {
+      Serial.print("Printing cycle k:");
+      Serial.println(t);
+      printTime("cycle"); 
+    }
     cycle(t);
     t = t+1;
     if(t>NTOTAL) {
       printing = false;
       Serial.println("Printing cycle ends.");
+      resetPrinter();
+      myStepper.step(-120);
     }
   }
 
-  // do things at start, boy
-  if(onetime) {
-    onetime = false;
+  // do things once at start, boy
+  if(goprint) {
+    goprint = false;
     resetPrinter();
     printImageData(sampleimage,432,1);
+    // plugging 24V POWER
+    setPinVal(VOLTSWITCH,HIGH);
   }
 }
 
 ////////////////////////////////////////////////////////
 // this is a clock cycle
 void cycle(int k) {
-  // CLOCK
-  delayMicroseconds(TCLOCK);
-  setPinVal(CLK,LOW);
+  //setPinVal(CLK,LOW);
 
   // first we send the DATA
-  if(k<NLAT) setPinVal(DATA, pixelValue(k-1) ? HIGH : LOW ); // k-1 cause we started at 1
-  else setPinVal(DATA,LOW);
+  if(k<NLAT) setPinVal(DATA, HIGH); // TEST ALL BLACK
+  //if(k<NLAT) setPinVal(DATA, pixelValue(k-1) ? HIGH : LOW ); // k-1 cause we started at 1
+  //else setPinVal(DATA,LOW);
 
   // every 432 dots, we will load the data using LATCH
-  if(k==NLAT) setPinVal(LAT,LOW);
-  else setPinVal(LAT,HIGH);
+  if(k==NLAT) {
+    setPinVal(LAT,LOW);
+    Serial.println("!!!!! LATCHED");
+  }
+  if(k==NLAT+4)
+    setPinVal(LAT,HIGH);
 
   // after all that, let's burn paper with the STROBES
   if(k==NLAT + NWAIT)
     setPinVal(STB1,LOW);
-  else
+  if(k==NLAT + NWAIT + NSTB)
     setPinVal(STB1,HIGH);
 
   if(k==NLAT + 2*NWAIT + NSTB)
     setPinVal(STB2,LOW);
-  else
+  if(k==NLAT + 2*NWAIT + 2*NSTB)
     setPinVal(STB2,HIGH);
 
   if(k==NLAT + 3*NWAIT + 2*NSTB)
     setPinVal(STB3,LOW);
-  else
+  if(k==NLAT + 3*NWAIT + 3*NSTB)
     setPinVal(STB3,HIGH);
 
   // CLOCK
   delayMicroseconds(TCLOCK);
-  setPinVal(CLK,HIGH);
+  setPinVal(CLK, k%2==0 ? HIGH : LOW);
 }
 
 ////////////////////////////////////////////////////////
@@ -142,16 +176,16 @@ boolean pixelValue(int k) { // k is in [0,431]
   uint8_t b = linedata[k/8];
   int gut = k%8;
   boolean res = (b & (1<<gut)) != 0; // if non-zero, the bit was set !
-  if(k<27) {
-    Serial.print("byte index:");
-    Serial.println(k/8);
-    Serial.print("the byte:");
-    Serial.println(b,HEX);
-    Serial.print("decalage:");    
-    Serial.println(gut);
-    Serial.print("------------------------ pixel value:");
-    Serial.println(res);
-  }
+  //  if(k<27) {
+  //    Serial.print("byte index:");
+  //    Serial.println(k/8);
+  //    Serial.print("the byte:");
+  //    Serial.println(b,HEX);
+  //    Serial.print("decalage:");    
+  //    Serial.println(gut);
+  //    Serial.print("------------------------ pixel value:");
+  //    Serial.println(res);
+  //  }
   return res;
 };
 
@@ -175,15 +209,15 @@ void printImageData(const uint8_t *image,int w,int h) {
   // let's suppose the image has exactly w=432, aka h*54 bytes in its data
   for(int rowStart=0; rowStart<h; rowStart+=54) {
     //int chunkHeight = ((h-rowStart) > 53) ? 53 : (h-rowStart);
-//    Serial.println("SAMPLE Linedata array was (0,1):");
-//    Serial.println(linedata[0]);
-//    Serial.println(linedata[1]);
+    //    Serial.println("SAMPLE Linedata array was (0,1):");
+    //    Serial.println(linedata[0]);
+    //    Serial.println(linedata[1]);
     // get linedata pointer
     *linedata = pgm_read_byte(image+rowStart);
-    Serial.println("SAMPLE SET Linedata array is now (first 3):");
+    /*Serial.println("SAMPLE SET Linedata array is now (first 3):");
     Serial.println(linedata[0]);
     Serial.println(linedata[1]);
-    Serial.println(linedata[2]);
+    Serial.println(linedata[2]);*/
     printLine();
     //    for (int i=0; i<((w/8)*chunkHeight); i++) {
     //      printLine( pgm_read_byte(image + (rowStart*(w/8)) + i) );
@@ -194,7 +228,8 @@ void printImageData(const uint8_t *image,int w,int h) {
 ////////////////////////////////////////////////////////
 void resetPrinter() {
   t = 1;
-  printing = false;
+  printing = false; // !! IMPORTANT
+  setPinVal(VOLTSWITCH,LOW); // !! IMPORTANT
   setPinVal(STB1,HIGH);
   setPinVal(STB2,HIGH);
   setPinVal(STB3,HIGH);
@@ -203,14 +238,49 @@ void resetPrinter() {
 }
 
 ////////////////////////////////////////////////////////
-void setPinVal(int pin, int val) {
+void setPinVal(int pin,int val) {
   //if(pin==STB1 || pin==STB2 || pin==STB3)
   if(debug) {
-    //Serial.println("Setting pin:",pin,val);
+    //Serial.print("Setting pin");
   }
   else {
-    //digitalWrite(pin,val);
+    if(pin==STB1 || pin==STB2 || pin==STB3) {
+      Serial.print(pin);
+      Serial.print(" STROBE: ");
+      Serial.println(val==1 ? "HIGH" : "LOW");
+      printTime("made strobe:");
+    }
+    digitalWrite(pin,val);
   }
+}
+
+////////////////////////////////////////////////////////
+// get the thermal head temp in C°
+double getTemperature(boolean verbose) {
+  // display initial temperature
+  int rawA = analogRead(tPin); // 0 to 1023
+  double Rk = 10.0/((1024.0/rawA)-1.0);
+  double T = 0;
+  if(Rk<40) T = 20;
+  if(Rk<30) T = 25;
+  if(Rk<20) T = 35;
+  if(Rk<10) T = 45;
+  if(Rk<5) T = 55;
+  if(verbose) {
+    Serial.print("Rk/Temperature: ");
+    Serial.print(Rk);
+    Serial.print(" / ");
+    Serial.println(T);
+  }
+  return T;
+}
+
+////////////////////////////////////////////////////////
+// printTime
+void printTime(String str) {
+  Serial.print(str);
+  Serial.print(" Time(milliseconds) is: ");
+  Serial.println((micros() - starttime)/1000.0);
 }
 
 ////////////////////////////////////////////////////////
@@ -224,6 +294,18 @@ void blink(int howManyTimes) {
     delay(200);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
