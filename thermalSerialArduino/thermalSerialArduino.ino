1ce2720fa7debbc6a3166c4dc00f71b0b63f0102
Pierre JdlF <pierre.jdlf@gmail.com>
2015-03-10 16:48:09 +0100

    kayboard

--- thermalSerialArduino/thermalSerialArduino.ino
+++ thermalSerialArduino/thermalSerialArduino.ino
@@ -1,6 +1,14 @@
 ///////////////////////////////////////////////////
 #include <Stepper.h>
 #include <avr/pgmspace.h>
+#include <PS2Keyboard.h>
+
+const int DataPin = 3;
+const int IRQpin = 2;
+
+PS2Keyboard keyboard;
+
+#define CHARRIOT 130
 
 #define buttonPin A0
 // thermal pins
@@ -12,7 +20,7 @@
 #define DOT5 11
 #define DOT6 12
 #define DOT7 13
-Stepper myStepper(40, 2, 3, 4, 5); 
+Stepper myStepper(40, A2, A3, 4, 5); 
 ///////////////////////////////////////////////////
 
 // HEAT DURATIONS
@@ -29,9 +37,8 @@ boolean aware = true;
 boolean receiving = false;
 int received = 0;
 unsigned char LINEDATA[8];
-
 boolean moving = false;
-
+int CURRENTCHARPOS = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -54,9 +61,14 @@ void setup() {
   digitalWrite(DOT6, LOW);
   digitalWrite(DOT7, LOW);
   myStepper.setSpeed(150);
+
+  keyboard.begin(DataPin, IRQpin, PS2Keymap_French);
+
   Serial.println("Ok. I'm Arduino");
   delay(2000);
   Serial.println("!START!");
+  
+  initHeadAtStartAndMargin();
 }
 
 // reset listener
@@ -71,6 +83,14 @@ void backToAware() {
 void loop() {
   //delay(2);
 
+  //////////////////////////////////////////////// KEYBOARD
+  if(keyboard.available()) {
+    // read the next key
+    char c = keyboard.read();
+    processChar(c);
+  }
+
+  //////////////////////////////////////////////// IMAGE
   if(aware) {
     if(Serial.available() > 0) {
       int in = Serial.read();
@@ -119,6 +139,80 @@ void loop() {
   }
 }
 
+byte a[5] = { B00110000, B01001010, B01001010, B01001010, B01111100 };
+byte b[5] = { B01111111, B01001000, B01001000, B01001000, B00110000 };
+byte c[5] = { B00111100, B01000010, B01000010, B01000010, B00100100 };
+byte d[5] = { B00111000, B01000100, B01000100, B01000100, B01111111 };
+byte e[5] = { B00111100, B01001010, B01001010, B01001010, B00101100 };
+byte f[5] = { B00001000, B01111100, B00001010, B00001010, B00000010 };
+
+byte g[5] = { B00111100, B01000010, B01010010, B01010010, B00110100 };
+byte h[5] = { B01111110, B01001000, B01001000, B01001000, B00110000 };
+byte i[5] = { B00000000, B00001000, B01111010, B00000000, B00000000 };
+byte j[5] = { B00000000, B01000000, B01001000, B00111010, B00000000 };
+byte k[5] = { B01111110, B00010000, B00101000, B01000100, B00000000 };
+
+byte l[5] = { B00000000, B00000010, B00111110, B01000000, B00000000 };
+byte m[5] = { B01111100, B00000100, B01111000, B00000100, B01111000 };
+byte n[5] = { B01111110, B01001000, B01001000, B01001000, B00110000 };
+byte o[5] = { B00111100, B01000010, B01000010, B01000010, B00111100 };
+byte p[5] = { B01111110, B00010010, B00010010, B00010010, B00001100 };
+
+byte q[5] = { B01111110, B01001000, B01001000, B01001000, B00110000 };
+byte r[5] = { B00110000, B01001000, B01001000, B01001000, B01111110 };
+byte s[5] = { B01001100, B01001010, B01010010, B01010010, B00110010 };
+byte t[5] = { B00000100, B00111111, B01000100, B01000100, B00000100 };
+byte u[5] = { B00111100, B01000000, B01000000, B01000000, B01111100 };
+
+byte v[5] = { B00001100, B00110000, B01000000, B00110000, B00001100 };
+byte w[5] = { B00011110, B01100000, B00011000, B01100000, B00011110 };
+byte x[5] = { B01000010, B00100100, B00011000, B00100100, B01000010 };
+byte y[5] = { B00100110, B01001000, B01001000, B01001000, B00111110 };
+byte z[5] = { B01000010, B01100010, B01010010, B01001010, B01000110 };
+
+const byte* alph[26] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+
+void processChar(char ch) {
+  
+  int charint = (int)ch;
+  if(charint>=97 && charint <=122) { // ALPHABET !
+    myStepper.step(-PAS);
+    printChar(alph[charint-97]);
+    CURRENTCHARPOS += 7;
+  }
+  
+  if(charint==32) { // SPACE
+    myStepper.step(-6);
+    CURRENTCHARPOS+=6;
+  }
+  
+  if(ch==PS2_ENTER || ch==PS2_DOWNARROW || CURRENTCHARPOS>350) {
+    initRetourCharriotAndMargin();
+    CURRENTCHARPOS = 0;
+  }
+  
+}
+
+void printChar(const byte *charlist) {
+  for(int vert=0; vert<5; vert=vert+1) {
+    for(int dot=0; dot<8; dot=dot+1) {
+      boolean on = charlist[vert] & (1 << dot);
+      int durat = 0;
+      if(on) {
+        durat = MAXHEATMICROS;
+        int pin = 13-dot;
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(durat);
+        digitalWrite(pin, LOW);
+      }
+    }
+    delay(10);
+    myStepper.step(-PAS);
+    delay(10);
+  }
+  myStepper.step(-PAS);
+}
+
 void printLine() {
   // let's print a line
   for(int dot=0; dot<8; dot=dot+1) {
@@ -131,11 +225,24 @@ void printLine() {
   myStepper.step(-PAS);
 }
 
+void initRetourCharriotAndMargin() {
+  if(CURRENTCHARPOS<CHARRIOT)
+    myStepper.step(-CHARRIOT+CURRENTCHARPOS);
+  initHeadAtStart();
+  myStepper.step(-20);
+}
+void initHeadAtStartAndMargin() {
+  initHeadAtStart();
+  myStepper.step(-20);
+}
 void initHeadAtStart() {
   while(analogRead(buttonPin) < 200) {
     myStepper.step(1);
   }
 }
+
+
+
 
 
 
